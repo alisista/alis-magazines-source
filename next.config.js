89a45018a34da5ecd87959a92bf2715bddf55ab8
OK Rabbit <ocrybit@gmail.com>
2018-12-05 13:23:43 +0100

    fixed webpack bug in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-const { parsed: localEnv } = require("dotenv").config()
-const webpack = require("webpack")
-module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-    return config
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? require("next/constants")
+    : require("next-server/constants")
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {
+      /* production only config */
+    }
+  }
+
+  const { parsed: localEnv } = require("dotenv").config()
+  const webpack = require("webpack")
+
+  return {
+    webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+      return config
+    }
   }
 }
